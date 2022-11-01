ea0b7cc3838c5477c781c1f13a00011dd4322e41
saksham1211 <saksham.dubey29@gmail.com>
2022-11-01 19:37:03 +0530

    first commit

---
+++ 1.two-sum.py
@@ -0,0 +1,33 @@
+
+
+
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+from ctypes.wintypes import tagRECT
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        dic={}
+        
+        for i in range(len(nums)):
+            ans=target-nums[i]
+
+            if ans not in dic:
+                dic[ans]=i
+
+        for i in range(len(nums)):
+            if ((target-nums[i]) in dic):
+                return [i, dic[target-nums[i]]]
+
+
+        
+        
+# @lc code=end
+
---
+++ 21.merge-two-sorted-lists.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=21 lang=python3
+#
+# [21] Merge Two Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from audioop import lin2adpcm
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if None in (list1, list2):
+            return list1 or list2
+        temp2=temp=ListNode(0)
+        while list1 and list2:
+            if list1.val < list2.val:
+
+                temp.next=list1
+                list1=list1.next
+
+            else:
+                temp.next=list2
+                list2=list2.next
+
+            temp=temp.next
+
+        temp.next=list1 or list2
+
+        return temp2.next
+
+
+
+
+# @lc code=end
+
